0269148050e570accaecc9acd03369299d93fe86
Marley <mrl.menard@gmail.com>
2024-06-12 11:26:59 +0200

    javascript quiz

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 function findSong() {
-    var div = document.getElementById("display_quiz_findsong");
-    div.style.display = "block";
+    var div_lyrics = document.getElementById("display_quiz_findlyrics");
+    div_lyrics.style.display = "none";
+
+    var div_song = document.getElementById("display_quiz_findsong");
+    div_song.style.display = "block";
 }
 
 function findLyrics() {
-    var div = document.getElementById("display_quiz_findlyrics");
-    div.style.display = "block";
+    var div_song = document.getElementById("display_quiz_findsong");
+    div_song.style.display = "none";
+
+    var div_lyrics = document.getElementById("display_quiz_findlyrics");
+    div_lyrics.style.display = "block";
 }
